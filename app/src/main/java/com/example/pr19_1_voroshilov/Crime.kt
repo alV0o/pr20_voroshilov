f2f69094d9cd64725b1be27087859a06cd91d78f
alV0o <voroshilov.olexandro07@mail.ru>
2025-10-07 18:14:21 +0500

    pr_20_voroshilov

---
+++ app/src/main/java/com/example/pr19_1_voroshilov/Crime.kt
@@ -0,0 +1,11 @@
+package com.example.pr19_1_voroshilov
+
+import java.text.DateFormat
+import java.util.Date
+import java.util.UUID
+
+data class Crime (val id: UUID = UUID.randomUUID()){
+    val date = Date()
+    var title = ""
+    var isSolved = false
+}
